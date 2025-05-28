b2eb57fa2a168f46389a562ccbc8d71f86c2cc81
tommasoravasio <161846779+tommasoravasio@users.noreply.github.com>
2025-05-28 18:55:29 +0200

    added correlation_comparison_job.py e run_correlation_comparison.sh

---
+++ jobs/run_network.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=network_job_tp53
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=06:00:00
+#SBATCH --output=network_job_output.log
+#SBATCH --error=network_job_error.log
+
+
+module load miniconda3
+eval "$(conda shell.bash hook)"
+conda activate tp53
+
+cd $HOME/tp53
+
+python jobs/network_job.py
+
