2811cf7e5e6cfa552cb46983a3248bbc10be5488
Tommaso Ravasio <tommyravam@gmail.com>
2025-05-28 18:55:29 +0200

    added correlation_comparison_job.py e run_correlation_comparison.sh

---
+++ jobs/run_correlation_comparison.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=correlation_tp53
+#SBATCH --output=correlation_tp53_%j.out
+#SBATCH --error=correlation_tp53_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=04:00:00
+
+
+
+module load miniconda3
+eval "$(conda shell.bash hook)"
+conda activate tp53
+cd $HOME/tp53
+python jobs/correlation_comparison_job.py
