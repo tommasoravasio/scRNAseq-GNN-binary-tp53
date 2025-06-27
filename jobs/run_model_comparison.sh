09b4e1339997fdc1519a78d28652ad5e311d143a
Tommaso Ravasio <tommyravam@gmail.com>
2025-06-27 11:15:19 +0200

    added hyperparameter tuning configs

--- jobs/run_model_comparison.sh
+++ jobs/run_model_comparison.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=model_comparison
-#SBATCH --output=results/model_comparison_output_%j.out
-#SBATCH --error=results/model_comparison_error_%j.err
+#SBATCH --output=model_comparison_output_%j.out
+#SBATCH --error=model_comparison_error_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
